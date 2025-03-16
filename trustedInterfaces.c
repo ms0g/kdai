d121d401fd1f32ba8480419aafdd4f47b051ad7f
Korel <Korelucpinar@gmail.com>
2025-03-16 13:01:42 -0400

    Add Comments and Minor updates to return values in trustedInterfaces.c
    Added comments to describe different functions in trustedInterfces.c. Also updated insert_trusted_interface to return an int depending on if the interface was succesfully added.
    
    Signed-off-by: Korel <Korelucpinar@gmail.com>

--- trustedInterfaces.c
+++ trustedInterfaces.c
@@ -4,61 +4,77 @@
 LIST_HEAD(trusted_interface_list);  // Global list head for interfaces
 static int trusted_list_size = 0;
 
-// Function to populate the list with all interfaces as trusted
+/**
+ * populate_trusted_interface_list - Populate the trusted interface list with all network interfaces.asm
+ * 
+ * This function iterates over all netowrk interfaces in the system and inserts each one into the
+ * the trusted interface list.
+ * 
+ */
 void populate_trusted_interface_list(void) {
     struct net_device *dev;
-    
     // Iterate over all network interfaces
     for_each_netdev(&init_net, dev) {
-        struct interface_entry *new_entry;
-
-        // Allocate memory for new entry, using kmalloc, the kernel equivalent of malloc
-        // GFP_KERNEL allows for blocking. Default for most kernel allocaitons
-        new_entry = kmalloc(sizeof(struct interface_entry), GFP_KERNEL);
-
-        //If memmory could not be allocated return
-        if (!new_entry) {
-            printk(KERN_ERR "kdai: Memory allocation failed for interface entry\n");
-            return;
-        }
-
-        // Copy interface name and add to list
-        strncpy(new_entry->name, dev->name, IFNAMSIZ - 1);
-        new_entry->name[IFNAMSIZ - 1] = '\0';  // Ensure null termination
-        list_add_tail(&new_entry->list, &trusted_interface_list);
-        trusted_list_size++;
+        insert_trusted_interface(dev->name);
     }
 }
 
-void insert_trusted_interface(const char *device_name) {
+/**
+ * insert_trusted_interface - Insert a new interface into the trusted list.
+ * @device_name: The name of the trusted interface to insert
+ * 
+ * This fucntion inserts the name of a network interface into teh trusted interface list. 
+ * It first checks if the interface already exists in the list using find_trusted_interface. 
+ * If the interface is not found it alloactes memory for a new entry, and cpoies the device name. 
+ * The list field is the intialized for the new entry and this fucntion adds the new entry to
+ * the end fo the lis tusing list_add_tail. The trusted_list_size if then incremented, and 
+ * a message is printed to indicate the enw addition.asm
+ * 
+ * Return: This fucntion returns 1 if the interface name was added, 0 if it already exists,
+ * and -1 if mmory allocaiton failed
+ */
+int insert_trusted_interface(const char *device_name) {
 
     struct interface_entry *new_entry;
 
     //If we found that device already return
     if(find_trusted_interface(device_name)){
-        return;
+        return 0;
     }
 
     // Allocate memory for the new entry
     new_entry = kmalloc(sizeof(struct interface_entry), GFP_KERNEL);
     if (!new_entry) {
         printk(KERN_ERR "Failed to allocate memory for interface entry\n");
-        return;
+        return -1;
     }
 
     // Copy the device name safely
     strncpy(new_entry->name, device_name, IFNAMSIZ - 1);
     new_entry->name[IFNAMSIZ - 1] = '\0'; // Ensure null termination
 
+    // Initialize the list field of the new entry
+    INIT_LIST_HEAD(&new_entry->list);
+
     // Add to the end of the list
     list_add_tail(&new_entry->list, &trusted_interface_list);
     trusted_list_size++;
 
     printk(KERN_INFO "Added interface: %s\n", new_entry->name);
+
+    return 1;
 }
 
-//Function to find an interface in the trusted list. Will return the device if the interface is trusted.
-//Will return NULL if the device was not found
+/**
+ * find_trusted_interface - Find an interface in the trusted list.
+ * @interface_name: The name of the interaface to find
+ * 
+ * This function searches the trusted interface list for an entry that matches the given interface name.
+ * If a matching entry is found, the function returns the name of th einterface. If no match is found the
+ * function returns NULL.
+ * 
+ * Return: The name of the trusted interface if found, or NULL if not found.
+ */
 const char* find_trusted_interface(const char *interface_name) {
     struct interface_entry *entry;
 
@@ -72,26 +88,46 @@ const char* find_trusted_interface(const char *interface_name) {
     return NULL; // Interface not found, return NULL
 }
 
-// Function to print all interfaces in the list
+/**
+ * print_trusted_interface_list - Print all interfaces in the trusted list
+ * 
+ * This function prints the names of all network interfaces in the trusted interface list.
+ * If the list is empty, it prints a message indicaitng that all interfaces are assumed to be Untrusted.
+ * 
+ * Return: This function does not return a value
+ */
 void print_trusted_interface_list(void) {
     struct interface_entry *entry;
 
     printk(KERN_INFO "kdai: List of trusted network interfaces:\n");
 
+    //If the list is empty notify the user
     if(trusted_list_size == 0) {
         printk(KERN_INFO "!!(The list is currently empty) All interfaces are assumed Untrusted!!\n");
         return;
     }
 
+    //Iterate and print each entry
     list_for_each_entry(entry, &trusted_interface_list, list) {
         printk(KERN_INFO " - %s\n", entry->name);
     }
 }
 
-// Function to free the list during module cleanup
+/**
+ * free_trusted_interface_list - Free all entries in the trusted interface list
+ * 
+ * This function iterates through the trusted interface list and frees each entry.
+ * It uses list_for_each_entry_safe to  traverse the list while deleting entries. 
+ * This means it uses an additional temporary pointer to store the next entry before 
+ * deleting the currnet one Each entry is removed from the list using list_del and 
+ * then freed using kfree.
+ * 
+ * Return: this function does not return anything
+ */
 void free_trusted_interface_list(void) {
     struct interface_entry *entry, *tmp;
 
+    //Iterate through the list, del and free each entry.
     list_for_each_entry_safe(entry, tmp, &trusted_interface_list, list) {
         list_del(&entry->list);
         kfree(entry);
--- trustedInterfaces.h
+++ trustedInterfaces.h
@@ -1,16 +1,20 @@
 #include "common.h"
 
-// Structure for storing interface names
+// Structure for storing trusted interface names
 struct interface_entry {
-    char name[IFNAMSIZ];    // Store interface name, IFNAMSIZ is a constant in the linux kernel
+    char name[IFNAMSIZ];    // Store trusted interface name
+                            // IFNAMSIZ is a constant in the linux kernel
                             // that defines the maximum length of a network interface
-                        
-    struct list_head list;  // Linked list pointer
+    struct list_head list;  // Point to the next item in the linked list 
 };
 
 // Function declarations
 void populate_trusted_interface_list(void);
-void insert_trusted_interface(const char *device_name);
+
+int insert_trusted_interface(const char *device_name);
+
 const char* find_trusted_interface(const char *interface_name);
+
 void print_trusted_interface_list(void);
+
 void free_trusted_interface_list(void);
